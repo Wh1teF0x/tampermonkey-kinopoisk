6dde0444c4121a6ad53ce8e9ebd93e5b1e14fa93
Wh1teF0x <untoxited@gmail.com>
2025-04-12 20:19:31 +0300

    fix: debug fix

--- script.js
+++ script.js
@@ -10,47 +10,52 @@
 // @grant GM_addStyle
 // @license MIT
 // ==/UserScript==
-function flicksbar() {
-  const url = window.location.href;
-  const kinopoiskId = url.match("/(series|film)/(?<id_kp>.+?)/");
-  const flickbarUrl = `https://flicksbar.mom/film/${kinopoiskId[2]}`;
+function attachRedirect() {
+  try {
+    const url = window.location.href;
+    const kinopoiskId = url.match('/(series|film)/(?<id_kp>.+?)/');
+    const flickbarUrl = `https://flicksbar.mom/film/${kinopoiskId[2]}`;
 
-  const playBtn = document.createElement("button");
+    const playBtn = document.createElement('button');
 
-  playBtn.type = "button";
-  playBtn.innerHTML = "Смотреть";
-  playBtn.classList.add("playBtn");
-  playBtn.onclick = () => window.open(`${flickbarUrl}`, "_blank");
+    playBtn.type = 'button';
+    playBtn.innerHTML = 'Смотреть';
+    playBtn.classList.add('playBtn');
+    playBtn.onclick = () => window.open(`${flickbarUrl}`, '_blank');
 
-  GM_addStyle(`
-    .playBtn {  
-      display: inline;
-      position: fixed;
-      z-index: 999999;
-      bottom: 30px;
-      right: 30px;
-      height: 44px;
-      padding: 9px 15px 9px 38px;
-      margin-right: 1px;
-      cursor: pointer;
+    GM_addStyle(`
+      .playBtn {  
+        display: inline;
+        position: fixed;
+        z-index: 999999;
+        bottom: 30px;
+        right: 30px;
+        height: 44px;
+        padding: 9px 15px 9px 38px;
+        margin-right: 1px;
+        cursor: pointer;
+        
+        border: none;
+        border-radius: 4.4px;
+        
+        font-family: Graphik Kinopoisk LC Web,Arial,Tahoma,Verdana,sans-serif;
+        font-size: 13px;
+        font-weight: 500;
+        color: #fff;
+        
+        background-color: #f60;
+        background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12' fill='%23fff'%3E %3Cpath fill-rule='evenodd' d='M3 1.3v9.4L10.311 6z'/%3E %3C/svg%3E");
+        background-position: 15px 13px;
+        background-size: 18px;
+        background-repeat: no-repeat;
+        }
+        `);
 
-      border: none;
-      border-radius: 4.4px;
-
-      font-family: Graphik Kinopoisk LC Web,Arial,Tahoma,Verdana,sans-serif;
-      font-size: 13px;
-      font-weight: 500;
-      color: #fff;
-
-      background-color: #f60;
-      background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12' fill='%23fff'%3E %3Cpath fill-rule='evenodd' d='M3 1.3v9.4L10.311 6z'/%3E %3C/svg%3E");
-      background-position: 15px 13px;
-      background-size: 18px;
-      background-repeat: no-repeat;
-    }
-  `);
-
-  document.body.appendChild(playBtn);
+    document.body.appendChild(playBtn);
+  } catch (e) {
+    console.log('UserScript: Something wrong');
+    console.error(e);
+  }
 }
 
-window.onload = flicksbar();
+window.addEventListener('load', attachRedirect);
